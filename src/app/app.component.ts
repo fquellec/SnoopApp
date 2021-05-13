9a2548ec6bc0ffc961ea735dfb7c6ca523b5f36f
Lou Richard <lrichard@openwt.com>
2021-05-13 20:08:40 +0200

    Add card element + d3 script

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
-  selector: 'ana-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'ana-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'AnaNewsApp';
+export class AppComponent implements OnInit {
+    title = 'Snoop';
+
+    constructor(
+        private _renderer2: Renderer2,
+        @Inject(DOCUMENT) private _document: Document
+    ) { }
+
+    public ngOnInit(): void {
+        let script = this._renderer2.createElement('script');
+        script.type = `application/json`;
+        script.src = "https://d3js.org/d3.v4.min.js";
+
+        this._renderer2.appendChild(this._document.body, script);
+    }
 }
