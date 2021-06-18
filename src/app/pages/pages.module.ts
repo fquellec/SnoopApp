1d55d339e54a8db5e0e17ae4daa68a20f0371f5d
Lou Richard <lrichard@openwt.com>
2021-06-18 21:35:06 +0200

    Add ngxspinner module

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CoreModule } from './../core/core.module';
+import { NgxSpinnerModule } from 'ngx-spinner';
 import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+import { CoreModule } from './../core/core.module';
 import { GraphsModule } from './../graphs/graphs.module';
 import { SamplePageComponent } from './components/sample-page/sample-page.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
@@ -16,8 +18,10 @@ import { MethodologyPageComponent } from './components/methodology-page/methodol
         CommonModule,
         CoreModule,
         GraphsModule,
+        NgxSpinnerModule,
         MatButtonModule,
         MatIconModule,
+        MatListModule,
     ],
     exports: [SamplePageComponent]
 })
